a84aa79f66530569d82430e079de1463feb5948b
Derek Alvarado <dmalvarado@gmail.com>
2016-10-19 18:46:46 -0500

    add a template file and a directive use a template

--- module1/js/app.js
+++ module1/js/app.js
@@ -3,7 +3,8 @@ angular.module('app', []);
 angular.module('app')
     .controller('mainCtrl', mainCtrl)
     .directive('userInfoCard', userInfoCard)
-    .directive('userInfoCardUsingScope', userInfoCardUsingScope);
+    .directive('userInfoCardUsingScope', userInfoCardUsingScope)
+    .directive('userInfoCardUsingTemplate', userInfoCardUsingTemplate);
 
 function mainCtrl($scope) {
     $scope.user = {
@@ -27,6 +28,14 @@ function userInfoCardUsingScope() {
         //Double curly items here will be available on the enclosing scope of controller
         //Note the use of !! as shorthand
         template: "Name: {{user.name}} <br/> <div ng-show='!!user.address.city'> City: {{user.address.city}} </div>",
+        restrict: "E",
+    }
+}
+
+function userInfoCardUsingTemplate() {
+    return {
+
+        templateUrl: "userInfoCardTemplate.html",
         restrict: "E",
     }
 }
