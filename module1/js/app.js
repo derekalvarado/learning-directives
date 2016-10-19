f83ce41f07c8bb36e5eab07bf8ff62a52b9393a4
Derek Alvarado <dmalvarado@gmail.com>
2016-10-19 18:34:28 -0500

    add new directive that takes advantage of controller scope objects

--- module1/js/app.js
+++ module1/js/app.js
@@ -2,15 +2,30 @@ angular.module('app', []);
 
 angular.module('app')
     .controller('mainCtrl', mainCtrl)
-    .directive('userInfoCard', userInfoCard);
-function mainCtrl() {
+    .directive('userInfoCard', userInfoCard)
+    .directive('userInfoCardUsingScope', userInfoCardUsingScope);
 
+function mainCtrl($scope) {
+    $scope.user = {
+        name: "Derek Alvarado",
+        address: {
+            city: "Austin, TX"
+        }
+    }
 }
 
 function userInfoCard() {
 
     return {
         template: "User Info Card",
+        restrict: "E",
+    }
+}
+
+function userInfoCardUsingScope() {
+    return {
+        //'name' will be available on the enclosing scope of controller
+        template: "Name: {{user.name}} <br/> City: {{user.address.city}}",
         restrict: "E",
     }
 }
